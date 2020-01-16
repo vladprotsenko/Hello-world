921c754aa2d72fc75abaf8d2563c495143684886
Vladimir Protsenko <vl@granit.kz>
2020-01-16 16:54:01 +0600

    First test-commit

---
+++ DataTime_test.py
@@ -0,0 +1,19 @@
+import datetime
+
+year, month, day = (int(i) for i in input().split())
+#with open('input.txt', 'r', encoding='utf-8 -sig' ) as fin:
+#    z = fin.readline().split()
+#    year = int(z[0])
+#    month = int(z[1])
+#    day = int(z[2])
+#    deltan = int(fin.readline())
+deltan = int(input())
+data = datetime.date(year, month, day)
+delta = datetime.timedelta(deltan)
+rez_data = data + delta
+
+print(int(rez_data.year), end=' ')
+#print(int(rez_data.month), end=' ')
+print(int(rez_data.day))
+
+#test change
